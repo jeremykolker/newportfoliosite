04e1292230d53ed8d21ab5a34c423ba6011da5bc
“Jeremy <jeremy.kolker@gmail.com>
2023-07-19 22:08:42 -0400

    new portfolio site

---
+++ my-app/src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "Jeremy Kolker Technologies";
+const meta = {
+    title: "Jeremy Kolker",
+    description: "SOFTWARE ENGINEER",
+    img:"https://i.imgur.com/o7IqWHQ.jpg",
+};
+
+const introdata = {
+    title: "Software Engineering & Web Development" + "\xa0",
+    animated: {
+        first: "I build useful apps",
+        second: "I code cool websites",
+        third: "I develop UX & UI",
+        fourth: "I help solve technical problems"
+    },
+    description: "Full-Stack Engineer",
+    your_img_url: "https://i.imgur.com/o7IqWHQ.jpg",
+};
+
+const dataabout = {
+    title: "About",
+    aboutme: "Throughout my life, I've worn many hats in a variety of different industries. No matter the job, constructing things that bring people joy is my passion. From working on architectural restoration projects in landmarked buildings all over New York City, to managing musical acts and performing worldwide as musician. These days I'm a Full Stack Software Developer and UX designer looking to code my next great masterpiece and make the digital world a better place. I strive on solving problems, being challenged, and working in teams. I specialize in Python, Javascript (including EJS, jQuery, and advanced JS tools), React, CSS, HTML, SQL, Django, Node, Mongo, and much more.",
+};
+
+const skills = [{
+        name: "Python",
+        value: 100,
+    },
+    {
+        name: "Django",
+        value: 100,
+    },
+    {
+        name: "JavaScript",
+        value: 100,
+    },
+    {
+        name: "React",
+        value: 100,
+    },
+    {
+        name: "JQuery",
+        value: 100,
+    },
+    {
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS/Bootstrap/Skeleton",
+        value: 100,
+    },
+    {
+        name: "PostgreSQL",
+        value: 100,
+    },
+];
+
+const services = [
+    {
+        title: "Systems Engineering",
+        description: "",
+    },{
+        title: "UI & UX Design",
+        description: "",
+    },
+    {
+        title: "Mobile Application Development",
+        description: "",
+    },
+    {
+        title: "Website Design & Management",
+        description: "",
+    },
+    {
+        title: "Technology Consulting",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.imgur.com/NmLHg3O.png",
+        // description: "Ziggy's Finds is a personalized e-commerce store with an engaging user-interface. Frontend built with React.js, consumes Django API, fully functional CRUD capabilities. Mobile ready styling built with CSS/Bootstrap. Anime.js used for JS animation styling.Backend built fully with Python, using a Postgresql Database Management system, serving JSON rest APIs.",
+        link: "https://github.com/jeremykolker/Ziggys-Finds",
+    },
+    {
+        img: "https://i.imgur.com/Po3UIkY.png",
+        // description: "SLASHR is a Horror Movie database, resource tool, and watchlist maker. Fullstack Crud App built using React, MongoDB, Mongo Atlas, and TMDB 3rd party API data.",
+        link: "https://github.com/forbesbrian96/Slashr_front-end",
+    },
+    {
+        img: "https://i.imgur.com/3t3hYiy.png",
+        // description: "To-Do It! Is a digital list-maker/organizer/calendar app that allows users to create and organize lists, schedules, and much more! A mobile ready Fullstack CRUD application built with Javascript(jQuery & EJS), CSS-Bootstrap styling, HTML-5, Node.JS, Express, and MongoDB for database management.",
+        link: "https://github.com/jeremykolker/ToDoIt",
+    },
+    {
+        img: "https://i.imgur.com/PDn22vi.png",
+        // description: "DOGGY DOG WORLD is a Dogwalking/Petcare web App that allows users (petcare professionals and dog walkers) to upload, manipulate, and save client data with a responsive interface that makes scheduling easy. Frontend built with React, consumes Django API, fully functional CRUD capabilities. Mobile ready styling built with CSS/Bootstrap.Backend built with Python and Postgresql Database Management, serving a JSON API.",
+        link: "https://github.com/tiffanysimione/Project-Four-Front-End-%20target=",
+    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jeremy.kolker@gmail.com",
+    YOUR_FONE: "(914)202-5801",
+    description: "Please contact me for any questions regarding my services. I look forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_zbos97m",
+    YOUR_TEMPLATE_ID: "template_nbdmfkn",
+    YOUR_USER_ID: "2xqonu7RZ9BZ8BV2V",
+};
+
+const socialprofils = {
+    github: "https://github.com/jeremykolker",
+    linkedin: "https://www.linkedin.com/in/jeremykolker/",
+ 
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    // worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
